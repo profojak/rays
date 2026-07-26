ce1fe8331da602d1ae84b8a24a5f6c37e9882dfa
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-07-26 08:38:16 +0200

    💎 Integrate `SDL3` window management

--- source/gui/main.cpp
+++ source/gui/main.cpp
@@ -1,3 +1,45 @@
+#include <SDL3/SDL.h>
+
 import rays;
 
-int main() { rays::greet(); }
+int main() {
+  rays::greet();
+
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("SDL_Init failed: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("Rays", 1280, 720, 0);
+  if (!window) {
+    SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+  if (!renderer) {
+    SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running = true;
+  while (running) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
+    }
+
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
