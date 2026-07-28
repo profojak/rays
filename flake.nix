7cc5e074276da10e3e8358c1d85b9bd18a7df289
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-07-28 17:19:31 +0200

    🪲 Fix `SDL3` runtime errors on Linux

--- flake.nix
+++ flake.nix
@@ -12,22 +12,32 @@
         pkgs = import nixpkgs { inherit system; };
         llvm = pkgs.llvmPackages_latest;
         stdenv = if pkgs.stdenv.isDarwin then pkgs.darwin else llvm.libcxxStdenv;
+        sdl-runtime = pkgs.lib.optionals (!pkgs.stdenv.isDarwin) ([
+          pkgs.wayland
+          pkgs.libxkbcommon
+          pkgs.libdecor
+          pkgs.mesa
+          pkgs.vulkan-loader
+        ]);
       in
       {
-        devShells.default = (pkgs.mkShell.override { inherit stdenv; }) {
+        devShells.default = (pkgs.mkShell.override { inherit stdenv; }) ({
           packages = [
             llvm.clang
             llvm.clang-tools
             pkgs.cmake
             pkgs.ninja
-          ];
+          ] ++ sdl-runtime;
 
           shellHook = ''
             echo "  $(clang --version | head -n1)"
             echo "  $(cmake --version | head -n1)"
             echo "  ninja version $(ninja --version)"
           '';
-        };
+        } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath sdl-runtime;
+          SDL_RENDER_DRIVER = "vulkan";
+        });
       }
     );
 }
